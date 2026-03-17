6b6d3cec51fc27790f7f4115e11e4aa0b3346451
Marlex05 <marlex040705@gmail.com>
2026-03-17 14:11:50 -0600

    Nombres footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,19 +2,31 @@ export default function Footer() {
   return (
     <footer className="bg-dark text-white mt-20">
 
-      <div className="max-w-xl mx-auto px-6 py-6 text-center">
+      <div className="max-w-6xl mx-auto px-6 py-10 flex flex-col md:flex-row gap-10">
 
-        <p className="text-lg font-semibold mb-2">
-          Proyecto Medio Ambiente
-        </p>
+        <div className="md:w-1/2 text-left">
+          <p className="text-lg font-semibold mb-2">
+            Proyecto Medio Ambiente
+          </p>
 
-        <p className="text-sm text-gray-300">
-          La química en la protección de la salud y el ambiente
-        </p>
+          <p className="text-sm text-gray-300">
+            La química en la protección de la salud y el ambiente
+          </p>
 
-        <p className="text-sm text-gray-400 mt-4">
-          © 2026 Proyecto escolar
-        </p>
+          <p className="text-sm text-gray-400 mt-4">
+            © 2026 Proyecto escolar
+          </p>
+        </div>
+
+        <div className="md:w-1/2 text-right space-y-2 text-sm text-gray-300">
+          <p>Karla Renata Barajas Márquez.</p>
+          <p>Ximena Carpio Méndez.</p>
+          <p>Emiliano Córtes Cázares.</p>
+          <p>Yosselyn García Escalera.</p>
+          <p>Elena Leticia Muñoz Silva.</p>
+          <p>Amanda Sánchez Soto.</p>
+          <p>Ayari Ximena Vigil Guerra.</p>
+        </div>
 
       </div>
 
